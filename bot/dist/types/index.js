161ff3ec0e53fbe263eba4220365a3eb2f4cfca8
freeman69rus <ilmir.halilov69rus69@gmail.com>
2022-11-18 05:55:10 +0300

    ts dist ignore

--- bot/dist/types/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Keyboard = void 0;
-var Keyboard;
-(function (Keyboard) {
-    Keyboard[Keyboard["NONE"] = 0] = "NONE";
-    Keyboard[Keyboard["MENU"] = 1] = "MENU";
-    Keyboard[Keyboard["BUTTONS"] = 2] = "BUTTONS";
-})(Keyboard = exports.Keyboard || (exports.Keyboard = {}));
